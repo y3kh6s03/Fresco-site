2b40d13cf1bfb238e5cd88e6702b528d15cda683
yosuke026 <y3kh6s03@gmail.com>
2023-09-21 15:12:35 +0900

    complete-recruit

--- app/Home/Home.js
+++ app/Home/Home.js
@@ -3,6 +3,7 @@ import { HomeAbout } from "@/app/Home/components/About/About"
 import { HomeFuture } from "@/app/Home/components/Future/Future"
 import { HomeService } from "@/app/Home/components/Service/Service"
 import { HomeWorks } from "@/app/Home/components/Works/Works"
+import { HomeRecruit } from "@/app/Home/components/Recriot/Recruit"
 
 export const Home = () => {
     return (
@@ -12,6 +13,7 @@ export const Home = () => {
             <HomeFuture/>
             <HomeService/>
             <HomeWorks/>
+            <HomeRecruit/>
         </>
     )
 }
