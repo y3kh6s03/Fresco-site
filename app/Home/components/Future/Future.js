ee0e17092f8b05bc8c2c01b1a418aa450e729a8e
yosuke026 <y3kh6s03@gmail.com>
2023-09-25 10:20:59 +0900

    fixed-media

--- app/Home/components/Future/Future.js
+++ app/Home/components/Future/Future.js
@@ -1,5 +1,6 @@
 "use client"
 
+import { LgButton } from "@/app/components/Button/Button"
 import "app/Home/components/Future/Future.scss"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
@@ -10,7 +11,9 @@ export const HomeFuture = () => {
     useEffect(() => {
 
         gsap.registerPlugin(ScrollTrigger);
-        // let tl = gsap.timeline();
+
+        const vh = window.innerHeight;
+        console.log(vh)
 
         gsap.to(".homeFuture__path", {
             clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
@@ -19,22 +22,24 @@ export const HomeFuture = () => {
 
             scrollTrigger: {
                 trigger: ".homeFuture",
-                start: "top-=25% bottom",
+                start: `top-=${.5 * vh} bottom`,
                 toggleActions: "play none none reverse",
-                markers: true
+                // markers: true
             }
         })
         gsap.to(".homeFuture__description__image", {
-            scale: 1.15,
+            scale: 1.2,
 
             scrollTrigger: {
-                trigger: ".homeFuture__path",
-                start: "top-=60% bottom",
+                trigger: ".homeFuture",
+                start: `top-=${.5 * vh} bottom`,
+                end: `bottom-=${.3 * vh} center`,
                 scrub: true,
                 markers: true
             }
         })
     }, [])
+
     return (
         <section className="homeFuture container">
             <div className="homeFuture__title contents__title">
@@ -51,12 +56,13 @@ export const HomeFuture = () => {
                         食を通じて、すこやかな未来をつくりたい
                     </h3>
                     <p className="homeFuture__description__p">
-                        食事からとる栄養は、生きていくために欠かせないものです。<br/>しかし、生きるために必要なことは
+                        食事からとる栄養は、生きていくために欠かせないものです。<br />しかし、生きるために必要なことは
                         食事だけではありません。健康づくりや地球環境保全など人生のためのあらゆる物事を、食事を
-                        通じてよりよくしていきたい。<br/>
+                        通じてよりよくしていきたい。<br />
                         フレスコカンパニーは、”食べる”を通じて今を生きる私たちや未来の
                         子供たちに笑顔あふれる人生を提供します。
                     </p>
+                    <LgButton href={"#"} buttonName={"Future"} />
                     <div className="homeFuture__description__image"></div>
                 </div>
             </div>
