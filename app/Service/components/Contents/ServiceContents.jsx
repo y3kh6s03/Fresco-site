e0db5cea4520df383b2bdbdfaea8d3e2bf49a450
yosuke026 <y3kh6s03@gmail.com>
2023-09-27 22:48:33 +0900

    service-complete

--- app/Service/components/Contents/ServiceContents.jsx
+++ app/Service/components/Contents/ServiceContents.jsx
@@ -1,40 +1,62 @@
+
+
+import { LgButton } from "@/app/components/Button/Button"
 import "app/Service/components/Contents/ServiceContents.scss"
+import serviceItems from "app/Service/components/Contents/ServiceContents.json"
 
 export const ServiceContents = () => {
+
+
+
     return (
-        <section className="serviceContents container">
-            <div className="serviceContents__title contents__title">
-                <h2 className="serviceContents__title__h2">
-                    Kyushoku
-                    <span className="serviceContents__title__h2__span">
-                        給食受託事業
-                    </span>
-                </h2>
-                <div className="serviceContents__title__image"></div>
-            </div>
+        <>
+            {
+                serviceItems.map((serviceItem) => {
+                    return (
+                        <section key={serviceItem.enTitle} className="serviceContents container">
+                            <div className="serviceContents__title contents__title">
+                                <h2 className="serviceContents__title__h2">
+                                    {serviceItem.enTitle}
+                                    <span className="serviceContents__title__h2__span">
+                                        {serviceItem.jaTitle}
+                                    </span>
+                                </h2>
+                                <div className="serviceContents__title__image"></div>
+                            </div>
 
-            <div className="serviceContents__inner">
-                <div className="serviceContents__inner__description">
-                    <div className="serviceContents__inner__description__headline">
-                        <h3>
-                            五感で食事を楽しむ
-                        </h3>
-                        <span>
-                            Enjoying through the five senses.
-                        </span>
-                    </div>
-                    <p className="serviceContents__inner__description__p">
-                        老健施設、特養施設、グループホーム、障害者支援施設などの施設へ給食を提供するサービスです。<br />
-                        栄養基準にのっとった献立の立案、食材発注、調理までを一気通貫して行っています。地元・長野の食材や四季折々の旬の食材をふんだんに使ったバラエティ豊かな日常の献立に加えて、季節の移り変わりを感じるイベント食、施設様に合わせた企画食などもご提案。<br />
-                        <br />
-                        おいしく食べることはもちろん、楽しく食べる時間を大切にする「五感で楽しむ食体験」を提供します。
-                    </p>
-                </div>
-                <div className="serviceContents__inner__image">
-                    <img src="/images/Home/kyushoku.jpg" alt="" />
-                </div>
-            </div>
+                            <div className="serviceContents__inner">
+                                <div className="serviceContents__inner__description">
+                                    <div className="serviceContents__inner__description__headline">
+                                        <h3>
+                                            {serviceItem.jaHeadline}
+                                        </h3>
+                                        <span>
+                                            {serviceItem.enHeadline}
+                                        </span>
+                                    </div>
+                                    <p className="serviceContents__inner__description__p">
+                                        {serviceItem.description.split("\n").map((splitItem, index) => {
+                                            return (
+                                                <>
+                                                    <span key={index}>
+                                                        {splitItem}
+                                                    </span>
+                                                    <br />
+                                                </>
+                                            )
+                                        })}
+                                    </p>
+                                    <LgButton href={serviceItem.href} buttonName={serviceItem.enTitle} />
+                                </div>
+                                <div className="serviceContents__inner__image">
+                                    <img src={serviceItem.image} alt={serviceItem.enTitle} />
+                                </div>
+                            </div>
+                        </section>
+                    )
+                })
 
-        </section>
+            }
+        </>
     )
 }
