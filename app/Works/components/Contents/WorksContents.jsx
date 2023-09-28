0a94844aeb336cf8e40126f8b7ca02268b1f64d4
yosuke026 <y3kh6s03@gmail.com>
2023-09-28 14:02:27 +0900

    works-complete

--- app/Works/components/Contents/WorksContents.jsx
+++ app/Works/components/Contents/WorksContents.jsx
@@ -1,30 +1,41 @@
 import "app/Works/components/Contents/WorksContents.scss";
+import workContentsItem from "app/Works/components/Contents/WorksContents.json"
 
 export const WorksContents = () => {
     return (
-        <section className="worksContents container">
-            <div className="worksContents__inner">
-                <div className="worksContents__inner__title">
-                    <h3 className="worksContents__inner__title__h3">
-                        SUPPORT
-                        <span>直営給食サポート</span>
-                    </h3>
-                    <div className="worksContents__inner__title__image">
-                        <img src="/images/Home/catering.jpg" alt="works-image" />
-                    </div>
-                </div>
-                <h4 className="worksContents__inner__headline">
-                    customer: <br />
-                    千曲市ワーケーションウエルカムデイズ
-                </h4>
-                <p className="worksContents__inner__description">
-                    千曲市観光局と株式会社ふろしきやが運営する「千曲市ワーケーションウェルカムデイズ」にて、出張シェフを実施。千曲市・あんずの里や姨捨の棚田などあらゆるロケーションで青空レストランを行いました。
-                </p>
-            </div>
+        <>
+            {
+                workContentsItem.map((workContentItem, index) => {
+                    return (
+                        <section key={index} className="worksContents container">
+                            <div className="worksContents__inner">
+                                <div className="worksContents__inner__title">
+                                    <h3 className="worksContents__inner__title__h3">
+                                        {workContentItem.enTitle}
+                                        <span>{workContentItem.jaTitle}</span>
+                                    </h3>
+                                    <div className="worksContents__inner__title__image">
+                                        <img src={workContentItem.image} alt="works-image" />
+                                    </div>
+                                </div>
+                                <h4 className="worksContents__inner__headline">
+                                    customer: <br />
+                                    {workContentItem.customer}&nbsp;さま
+                                </h4>
+                                <p className="worksContents__inner__description">
+                                    {workContentItem.description}
+                                </p>
+                            </div>
 
-            <div src="" alt="" className="worksContents__subimage" >
-                <img src="/images/Home/catering3.jpg" alt="works-image" />
-            </div>
-        </section>
+                            <div src="" alt="" className="worksContents__subimage" >
+                                <img src={workContentItem.subImage} alt="works-image" />
+                            </div>
+                        </section>
+
+                    )
+                })
+
+            }
+        </>
     )
 }
