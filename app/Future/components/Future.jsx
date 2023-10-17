234d8798b67e339450d6877f98901ca582fec226
yosuke026 <y3kh6s03@gmail.com>
2023-10-17 13:20:52 +0900

    future completed

--- app/Future/components/Future.jsx
+++ app/Future/components/Future.jsx
@@ -8,6 +8,7 @@ import { Content } from "@/app/Future/components/Content/Content"
 // import { SecondContent } from "@/app/Future/components/SecondContent/SecondContent"
 // import { ThirdContent } from "@/app/Future/components/ThirdContent/ThirdContent"
 import { SubPageHero } from "@/app/components/SubPageHero/SubPageHero"
+import { ContactButton } from "@/app/components/Button/Button"
 
 export const Future = () => {
 
@@ -18,29 +19,28 @@ export const Future = () => {
         headline: ""
     }
 
-    const firstRef = useRef(null);
-    const secondRef = useRef(null);
-    const thirdRef = useRef(null);
-    const { scrollYProgress: scrollYProgress1 } = useScroll({
-        target: firstRef
-    });
-    const { scrollYProgress: scrollYProgress2 } = useScroll({
-        target: secondRef
-    });
-    const { scrollYProgress: scrollYProgress3 } = useScroll({
-        target: thirdRef
-    });
-    const transformYValue1 = useTransform(scrollYProgress1, [0, 1], [0, 30])
-    const transformYValue2 = useTransform(scrollYProgress2, [0, 1], [0, 50])
-    const transformYValue3 = useTransform(scrollYProgress3, [0, 1], [0, 50])
-    const transformXValue1 = useTransform(scrollYProgress1, [0, 1], [0, 20])
+    // const firstRef = useRef(null);
+    // const secondRef = useRef(null);
+    // const thirdRef = useRef(null);
+    // const { scrollYProgress: scrollYProgress1 } = useScroll({
+    //     target: firstRef
+    // });
+    // const { scrollYProgress: scrollYProgress2 } = useScroll({
+    //     target: secondRef
+    // });
+    // const { scrollYProgress: scrollYProgress3 } = useScroll({
+    //     target: thirdRef
+    // });
+    // const transformYValue1 = useTransform(scrollYProgress1, [0, 1], [0, 30])
+    // const transformYValue2 = useTransform(scrollYProgress2, [0, 1], [0, 50])
+    // const transformYValue3 = useTransform(scrollYProgress3, [0, 1], [0, 50])
+    // const transformXValue1 = useTransform(scrollYProgress1, [0, 1], [0, 20])
 
     return (
         <>
             <SubPageHero item={FutureHeroItem} />
-            <Content ref={firstRef} transformXValue={transformXValue1} transformYValue={transformYValue1} />
-            {/* <SecondContent ref={secondRef} transformYValue={transformYValue2} />
-            <ThirdContent ref={thirdRef} transformYValue={transformYValue3} /> */}
+            <Content/>
+            <ContactButton/>
         </>
     )
 }
